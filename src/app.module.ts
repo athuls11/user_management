1c762a293d9ca131c4517c54b48001c71f837984
athuls11 <athuls180@gmail.com>
2024-06-07 12:19:27 +0530

    Install required libraries and establish database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_DB_URI),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
