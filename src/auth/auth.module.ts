5bb62ac73882ff4791808f91dd9678f0f3ab7d31
athuls11 <athuls180@gmail.com>
2024-06-07 13:19:18 +0530

    Implement JWT functionality

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './services/auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async generateAccessToken(email: string, id: string): Promise<string> {
+    let payload = { email, id };
+    return this.jwtService.signAsync(payload);
+  }
+}
